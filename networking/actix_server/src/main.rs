3b1ab5f619070fb910c472c0fa74f9e84903fbf4
ayush <ayushagarwal.dev@gmail.com>
2026-01-11 00:15:44 +0530

    refactor: convert core-api into lib and add api mapping into networking folder

--- networking/actix_server/src/main.rs
+++ networking/actix_server/src/main.rs
@@ -3,6 +3,8 @@ use actix_web::{
     web::{self, route},
 };
 
+mod api;
+
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", name)
