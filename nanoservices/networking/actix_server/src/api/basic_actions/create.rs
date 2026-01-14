22e012360a0de5d71fa4eeb84c0ea26d365de731
ayush <ayushagarwal.dev@gmail.com>
2026-01-14 20:59:08 +0530

    add: token extraction middleware from headers

--- nanoservices/networking/actix_server/src/api/basic_actions/create.rs
+++ nanoservices/networking/actix_server/src/api/basic_actions/create.rs
@@ -1,9 +1,14 @@
 use actix_web::{HttpResponse, web::Json};
 use glue::errors::NanoServiceError;
+use glue::token::HeaderToken;
 use to_do_core::api::basic_actions::{create::create as create_core, get::get_all as get_all_core};
 use to_do_core::structs::ToDoItem;
 
-pub async fn create(body: Json<ToDoItem>) -> Result<HttpResponse, NanoServiceError> {
+pub async fn create(
+    token: HeaderToken,
+    body: Json<ToDoItem>,
+) -> Result<HttpResponse, NanoServiceError> {
+    println!("Token: {}", token.message);
     let _ = create_core(body.into_inner()).await?;
     Ok(HttpResponse::Ok().json(get_all_core().await?))
 }
