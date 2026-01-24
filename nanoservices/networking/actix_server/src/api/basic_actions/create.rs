7a4b9490ab9342ab147422c5f079df34a8ce0b47
ayush <ayushagarwal.dev@gmail.com>
2026-01-25 04:23:03 +0530

    update: server api calls for sqlx fns and connect to server route

--- nanoservices/networking/actix_server/src/api/basic_actions/create.rs
+++ nanoservices/networking/actix_server/src/api/basic_actions/create.rs
@@ -1,14 +1,17 @@
-use actix_web::{HttpResponse, web::Json};
-use glue::errors::NanoServiceError;
-use glue::token::HeaderToken;
 use to_do_core::api::basic_actions::{create::create as create_core, get::get_all as get_all_core};
-use to_do_core::structs::ToDoItem;
 
-pub async fn create(
+use actix_web::{HttpResponse, web::Json};
+use glue::{errors::NanoServiceError, token::HeaderToken};
+use to_do_dal::to_do_items::{
+    schema::NewToDoItem,
+    transactions::{create::SaveOne, get::GetAll},
+};
+
+pub async fn create<T: SaveOne + GetAll>(
     token: HeaderToken,
-    body: Json<ToDoItem>,
+    body: Json<NewToDoItem>,
 ) -> Result<HttpResponse, NanoServiceError> {
     println!("Token: {}", token.message);
-    let _ = create_core(body.into_inner()).await?;
-    Ok(HttpResponse::Ok().json(get_all_core().await?))
+    let _ = create_core::<T>(body.into_inner()).await?;
+    Ok(HttpResponse::Created().json(get_all_core::<T>().await?))
 }
--- nanoservices/networking/actix_server/src/api/basic_actions/update.rs
+++ nanoservices/networking/actix_server/src/api/basic_actions/update.rs
@@ -1,9 +1,14 @@
 use actix_web::{HttpResponse, web::Json};
 use glue::errors::NanoServiceError;
 use to_do_core::api::basic_actions::{get::get_all as get_all_core, update::update as update_core};
-use to_do_core::structs::ToDoItem;
 
-pub async fn update(body: Json<ToDoItem>) -> Result<HttpResponse, NanoServiceError> {
-    update_core(body.into_inner()).await?;
-    Ok(HttpResponse::Ok().json(get_all_core().await?))
+use to_do_dal::to_do_items::schema::ToDoItem;
+use to_do_dal::to_do_items::transactions::get::GetAll;
+use to_do_dal::to_do_items::transactions::update::UpdateOne;
+
+pub async fn update<T: UpdateOne + GetAll>(
+    body: Json<ToDoItem>,
+) -> Result<HttpResponse, NanoServiceError> {
+    update_core::<T>(body.into_inner()).await?;
+    Ok(HttpResponse::Ok().json(get_all_core::<T>().await?))
 }
