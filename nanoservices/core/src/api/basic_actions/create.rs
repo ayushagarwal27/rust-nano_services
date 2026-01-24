b568ef8b7de01c999a24c5171675678ba9c8a5b8
ayush <ayushagarwal.dev@gmail.com>
2026-01-24 15:45:59 +0530

    connnect: transaction to core calls

--- nanoservices/core/src/api/basic_actions/create.rs
+++ nanoservices/core/src/api/basic_actions/create.rs
@@ -1,9 +1,10 @@
-use to_do_dal::json_file::save_one;
-
-use crate::{enums::TaskStatus, structs::ToDoItem};
 use glue::errors::NanoServiceError;
+use to_do_dal::to_do_items::{
+    schema::{NewToDoItem, ToDoItem},
+    transactions::create::SaveOne,
+};
 
-pub async fn create(item: ToDoItem) -> Result<ToDoItem, NanoServiceError> {
-    let _ = save_one(&item.title.to_string(), &item)?;
-    Ok(item)
+pub async fn create<T: SaveOne>(item: NewToDoItem) -> Result<ToDoItem, NanoServiceError> {
+    let created_item = T::save_one(item).await?;
+    Ok(created_item)
 }
--- nanoservices/dal/src/to_do_items/schema.rs
+++ nanoservices/dal/src/to_do_items/schema.rs
@@ -1,3 +1,4 @@
+use glue::errors::NanoServiceError;
 use serde::{Deserialize, Serialize};
 
 use super::enums::TaskStatus;
@@ -14,4 +15,25 @@ pub struct ToDoItem {
     pub id: i32,
     pub title: String,
     pub status: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct AllToDoItems {
+    pub pending: Vec<ToDoItem>,
+    pub done: Vec<ToDoItem>,
+}
+
+impl AllToDoItems {
+    pub fn from_vec(all_items: Vec<ToDoItem>) -> Result<AllToDoItems, NanoServiceError> {
+        let mut pending = Vec::new();
+        let mut done = Vec::new();
+
+        for item in all_items {
+            match TaskStatus::from_string(&item.status).unwrap() {
+                TaskStatus::PENDING => pending.push(item),
+                TaskStatus::DONE => done.push(item),
+            }
+        }
+        Ok(AllToDoItems { pending, done })
+    }
 }
